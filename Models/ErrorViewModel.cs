6cfc8f8c061e6e715f2376710020d0f1f48e6b2e
SketchySpaghetti <jamiekaiparker@gmail.com>
2025-05-08 11:19:35 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROG7311_POE_Part2_ST10257863.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
