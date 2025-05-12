5fd6a970376601d637118387f43ab6b8bd4358ea
SketchySpaghetti <jamiekaiparker@gmail.com>
2025-05-12 15:54:52 +0200

    Cleaned up Program file, adding headers and region

--- Program.cs
+++ Program.cs
@@ -4,34 +4,32 @@ using PROG7311_POE_Part2_ST10257863.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Retrieve the connection string from appsettings.json
+// --- Configure Services ---
+
+// Database context
 var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");
-
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-	options.UseSqlServer(connectionString)); // Use the connection string from appsettings
+	options.UseSqlServer(connectionString));
 
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
-
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//	.AddEntityFrameworkStores<ApplicationDbContext>();
+// Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 	options.SignIn.RequireConfirmedAccount = false)
 	.AddEntityFrameworkStores<ApplicationDbContext>()
 	.AddDefaultTokenProviders();
 
-// Add services to the container.
+// MVC & Razor Pages
 builder.Services.AddControllersWithViews();
 
+// Authorization policies
 builder.Services.AddAuthorizationBuilder()
 	.AddPolicy("RequireEmployeeRole", policy => policy.RequireRole("Employee"))
 	.AddPolicy("RequireFarmerRole", policy => policy.RequireRole("Farmer"))
 	.AddPolicy("RequireCustomerRole", policy => policy.RequireRole("Customer"));
 
-
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// --- Configure Middleware ---
+
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Home/Error");
@@ -50,27 +48,32 @@ app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//Seed roles and users
-
-
-//using (var scope = app.Services.CreateScope())
-//{
-//	var services = scope.ServiceProvider;
-//	await SeedRolesAsync(services);
-//}
+// --- Optional: Seed roles and users ---
+// To enable, uncomment the following lines and the SeedRolesAsync method below.
+/*
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await SeedRolesAsync(services);
+}
+*/
 
 app.Run();
 
-//async Task SeedRolesAsync(IServiceProvider serviceProvider)
-//{
-//	var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-//	string[] roleNames = { "Employee", "Farmer", "Customer" };
+#region Optional: Role Seeding
+/*
+async Task SeedRolesAsync(IServiceProvider serviceProvider)
+{
+    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    string[] roleNames = { "Employee", "Farmer", "Customer" };
 
-//	foreach (var roleName in roleNames)
-//	{
-//		if (!await roleManager.RoleExistsAsync(roleName))
-//		{
-//			await roleManager.CreateAsync(new IdentityRole(roleName));
-//		}
-//	}
-//}
+    foreach (var roleName in roleNames)
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
+*/
+#endregion
